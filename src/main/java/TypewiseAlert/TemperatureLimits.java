8b51a3b40641c06c61b78a532224b8886f5cc0ff
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 13:45:10 +0530

    Update TemperatureLimits.java

--- src/main/java/TypewiseAlert/TemperatureLimits.java
+++ src/main/java/TypewiseAlert/TemperatureLimits.java
@@ -1,26 +1,23 @@
 package TypewiseAlert;
 
-public class TemperatureLimits {
-    public static double[] getLimits(CoolingType coolingType) {
-        double lowerLimit;
-        double upperLimit;
+import java.util.HashMap;
+import java.util.Map;
 
-        switch (coolingType) {
-            case PASSIVE_COOLING:
-                lowerLimit = 0;
-                upperLimit = 35;
-                break;
-            case HI_ACTIVE_COOLING:
-                lowerLimit = 0;
-                upperLimit = 45;
-                break;
-            case MED_ACTIVE_COOLING:
-                lowerLimit = 0;
-                upperLimit = 40;
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown CoolingType");
+public class TemperatureLimits {
+
+    private static final Map<CoolingType, double[]> limitsMap = new HashMap<>();
+
+    static {
+        limitsMap.put(CoolingType.PASSIVE_COOLING, new double[]{0, 35});
+        limitsMap.put(CoolingType.HI_ACTIVE_COOLING, new double[]{0, 45});
+        limitsMap.put(CoolingType.MED_ACTIVE_COOLING, new double[]{0, 40});
+    }
+
+    public static double[] getLimits(CoolingType coolingType) {
+        if (limitsMap.containsKey(coolingType)) {
+            return limitsMap.get(coolingType);
+        } else {
+            throw new IllegalArgumentException("Unknown CoolingType");
         }
-        return new double[]{lowerLimit, upperLimit};
     }
 }
