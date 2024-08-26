a3d77ad5d99ab33db2a62880b42eeb81ca780cea
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 13:37:23 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,36 +1,40 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class TypewiseAlertTest {
-
-    @Test
-    public void testInferBreach() {
-        assertEquals(TypewiseAlert.BreachType.TOO_LOW, TypewiseAlert.inferBreach(-5, 0, 35));
-        assertEquals(TypewiseAlert.BreachType.TOO_HIGH, TypewiseAlert.inferBreach(50, 0, 35));
-        assertEquals(TypewiseAlert.BreachType.NORMAL, TypewiseAlert.inferBreach(20, 0, 35));
-    }
+public class BreachClassifierTest {
 
     @Test
     public void testClassifyTemperatureBreach() {
-        assertEquals(TypewiseAlert.BreachType.TOO_LOW, TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.PASSIVE_COOLING, -5));
-        assertEquals(TypewiseAlert.BreachType.TOO_HIGH, TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING, 50));
-        assertEquals(TypewiseAlert.BreachType.NORMAL, TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.MED_ACTIVE_COOLING, 30));
+        assertEquals(BreachType.TOO_LOW, BreachClassifier.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, -5));
+        assertEquals(BreachType.TOO_HIGH, BreachClassifier.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 50));
+        assertEquals(BreachType.NORMAL, BreachClassifier.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 30));
     }
+}
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AlertSenderTest {
 
     @Test
-    public void testCheckAndAlert() {
-        TypewiseAlert.BatteryCharacter batteryChar = new TypewiseAlert().new BatteryCharacter();
-        batteryChar.coolingType = TypewiseAlert.CoolingType.PASSIVE_COOLING;
-
+    public void testSendToController() {
         // Redirect stdout to capture output for testing
         java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
         System.setOut(new java.io.PrintStream(outContent));
 
-        TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_CONTROLLER, batteryChar, -5);
+        AlertSender.sendAlert(AlertTarget.TO_CONTROLLER, BreachType.TOO_LOW);
         assertTrue(outContent.toString().contains("feed : TOO_LOW"));
 
-        outContent.reset();
-        TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_EMAIL, batteryChar, 50);
+        System.setOut(System.out); // Reset System.out
+    }
+
+    @Test
+    public void testSendToEmail() {
+        // Redirect stdout to capture output for testing
+        java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(outContent));
+
+        AlertSender.sendAlert(AlertTarget.TO_EMAIL, BreachType.TOO_HIGH);
         assertTrue(outContent.toString().contains("Hi, the temperature is too high"));
 
         System.setOut(System.out); // Reset System.out
