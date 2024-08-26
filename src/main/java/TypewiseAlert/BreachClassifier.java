5ad79cb27507030de350d9b523f6126f42e72e6d
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 13:36:38 +0530

    Create BreachClassifier.java

---
+++ src/main/java/TypewiseAlert/BreachClassifier.java
@@ -0,0 +1,18 @@
+package TypewiseAlert;
+
+public class BreachClassifier {
+    public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+        double[] limits = TemperatureLimits.getLimits(coolingType);
+        return inferBreach(temperatureInC, limits[0], limits[1]);
+    }
+
+    private static BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+        if (value < lowerLimit) {
+            return BreachType.TOO_LOW;
+        }
+        if (value > upperLimit) {
+            return BreachType.TOO_HIGH;
+        }
+        return BreachType.NORMAL;
+    }
+}
