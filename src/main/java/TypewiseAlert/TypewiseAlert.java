daee4c680ce19c5451d81da56047f59b63453b38
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 14:10:23 +0530

    Update TypewiseAlert.java

--- src/main/java/TypewiseAlert/TypewiseAlert.java
+++ src/main/java/TypewiseAlert/TypewiseAlert.java
@@ -1,10 +1,21 @@
 package TypewiseAlert;
 
 public class TypewiseAlert {
+
+
+
     public static void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
         BreachType breachType = BreachClassifier.classifyTemperatureBreach(
             batteryChar.getCoolingType(), temperatureInC
         );
-        AlertSender.sendAlert(alertTarget, breachType);
+
+        switch (alertTarget) {
+            case TO_CONTROLLER:
+                AlertSender.sendToController(breachType);
+                break;
+            case TO_EMAIL:
+                AlertSender.sendToEmail(breachType);
+                break;
+        }
     }
 }
