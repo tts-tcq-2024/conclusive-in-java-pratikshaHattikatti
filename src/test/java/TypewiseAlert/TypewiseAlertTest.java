c7a42f2c2d88d61f0cf32b22d8053c9cc454200f
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 14:52:23 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,19 +1,1 @@
-package TypewiseAlert;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-
-public class TemperatureLimitsTest {
-
-    @Test
-    public void testGetLimits() {
-        assertArrayEquals(new int[]{0, 35}, TemperatureLimits.getLimits(CoolingType.PASSIVE_COOLING));
-        assertArrayEquals(new int[]{0, 45}, TemperatureLimits.getLimits(CoolingType.HI_ACTIVE_COOLING));
-        assertArrayEquals(new int[]{0, 40}, TemperatureLimits.getLimits(CoolingType.MED_ACTIVE_COOLING));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetLimitsUnknownType() {
-        TemperatureLimits.getLimits(null); // Test with null or an invalid type if applicable
-    }
-}
