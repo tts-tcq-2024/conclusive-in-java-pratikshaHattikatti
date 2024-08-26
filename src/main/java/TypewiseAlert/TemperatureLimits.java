d902436734573e0425848938d176709c35dcd8f6
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 13:36:19 +0530

    Create TemperatureLimits.java

---
+++ src/main/java/TypewiseAlert/TemperatureLimits.java
@@ -0,0 +1,16 @@
+package TypewiseAlert;
+
+public class TemperatureLimits {
+    public static double[] getLimits(CoolingType coolingType) {
+        switch (coolingType) {
+            case PASSIVE_COOLING:
+                return new double[]{0, 35};
+            case HI_ACTIVE_COOLING:
+                return new double[]{0, 45};
+            case MED_ACTIVE_COOLING:
+                return new double[]{0, 40};
+            default:
+                throw new IllegalArgumentException("Unknown CoolingType");
+        }
+    }
+}
