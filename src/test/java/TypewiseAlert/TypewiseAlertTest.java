83f6967ffd785f595f77a8cdeb81afd54fd9bd26
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 13:25:25 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,1 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TypewiseAlertTest {
+
+    @Test
+    public void testInferBreach() {
+        assertEquals(TypewiseAlert.BreachType.TOO_LOW, TypewiseAlert.inferBreach(-5, 0, 35));
+        assertEquals(TypewiseAlert.BreachType.TOO_HIGH, TypewiseAlert.inferBreach(50, 0, 35));
+        assertEquals(TypewiseAlert.BreachType.NORMAL, TypewiseAlert.inferBreach(20, 0, 35));
+    }
+
+    @Test
+    public void testClassifyTemperatureBreach() {
+        assertEquals(TypewiseAlert.BreachType.TOO_LOW, TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.PASSIVE_COOLING, -5));
+        assertEquals(TypewiseAlert.BreachType.TOO_HIGH, TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING, 50));
+        assertEquals(TypewiseAlert.BreachType.NORMAL, TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.MED_ACTIVE_COOLING, 30));
+    }
+
+    @Test
+    public void testCheckAndAlert() {
+        TypewiseAlert.BatteryCharacter batteryChar = new TypewiseAlert().new BatteryCharacter();
+        batteryChar.coolingType = TypewiseAlert.CoolingType.PASSIVE_COOLING;
+
+        // Verify console output or use mocking framework to verify methods are called
+    }
+}
 
