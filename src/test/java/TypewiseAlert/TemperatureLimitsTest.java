0609463adcd2be59170f977635e4891d4dd1ac66
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 14:41:35 +0530

    Update TemperatureLimitsTest.java

--- src/test/java/TypewiseAlert/TemperatureLimitsTest.java
+++ src/test/java/TypewiseAlert/TemperatureLimitsTest.java
@@ -7,9 +7,9 @@ public class TemperatureLimitsTest{
 
     @Test
     public void testGetLimits() {
-        assertArrayEquals(new int[]{0, 35}, TemperatureLimits.getLimits(CoolingType.PASSIVE_COOLING));
-        assertArrayEquals(new int[]{0, 45}, TemperatureLimits.getLimits(CoolingType.HI_ACTIVE_COOLING));
-        assertArrayEquals(new int[]{0, 40}, TemperatureLimits.getLimits(CoolingType.MED_ACTIVE_COOLING));
+        assertArrayEquals(new double[]{0.0, 35.0}, TemperatureLimits.getLimits(CoolingType.PASSIVE_COOLING));
+        assertArrayEquals(new double[]{0.0, 45.0}, TemperatureLimits.getLimits(CoolingType.HI_ACTIVE_COOLING));
+        assertArrayEquals(new double[]{0.0, 40.0}, TemperatureLimits.getLimits(CoolingType.MED_ACTIVE_COOLING));
     }
 
     @Test(expected = IllegalArgumentException.class)
