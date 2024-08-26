7cf5cf3af9d7380492c822c545e0cac404b8ab68
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-26 14:49:19 +0530

    Update TemperatureLimitsTest.java

--- src/test/java/TypewiseAlert/TemperatureLimitsTest.java
+++ src/test/java/TypewiseAlert/TemperatureLimitsTest.java
@@ -3,13 +3,15 @@ package TypewiseAlert;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class TemperatureLimitsTest{
+public class TemperatureLimitsTest {
+
+    private static final double DELTA = 1e-15; // Tolerance for floating-point comparison
 
     @Test
     public void testGetLimits() {
-        assertArrayEquals(new double[]{0.0, 35.0}, TemperatureLimits.getLimits(CoolingType.PASSIVE_COOLING));
-        assertArrayEquals(new double[]{0.0, 45.0}, TemperatureLimits.getLimits(CoolingType.HI_ACTIVE_COOLING));
-        assertArrayEquals(new double[]{0.0, 40.0}, TemperatureLimits.getLimits(CoolingType.MED_ACTIVE_COOLING));
+        assertArrayEquals(new double[]{0.0, 35.0}, TemperatureLimits.getLimits(CoolingType.PASSIVE_COOLING), DELTA);
+        assertArrayEquals(new double[]{0.0, 45.0}, TemperatureLimits.getLimits(CoolingType.HI_ACTIVE_COOLING), DELTA);
+        assertArrayEquals(new double[]{0.0, 40.0}, TemperatureLimits.getLimits(CoolingType.MED_ACTIVE_COOLING), DELTA);
     }
 
     @Test(expected = IllegalArgumentException.class)
